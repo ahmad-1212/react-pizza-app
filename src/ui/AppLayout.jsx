a57cb459c90af786299079c90417619416c9e539
ahmad1212 <ahmadali.swat333@gmail.com>
2023-09-13 00:06:06 +0500

    initial-commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,25 @@
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
+
+const AppLayout = () => {
+  const navigations = useNavigation();
+  const isLoading = navigations.state === 'loading';
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
+      <Header />
+
+      <div className="overflow-y-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
